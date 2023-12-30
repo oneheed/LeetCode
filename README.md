c5a81b2dfeca12c01f62c362b036262ac7e4ee79
PuTaoNiNi <oneheed@gmail.com>
2023-12-30 16:19:01 +0800

    Add 1897. Redistribute Characters to Make All Strings Equal

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 
 ## 參考資料
 [LABULADONG 的算法网站](https://labuladong.github.io/algo/)
-
-#
---
+++ solution/01897. Redistribute Characters to Make All Strings Equal/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+var solution = new Solution();
+
+string[] words = ["abc", "aabc", "bc"];
+var output = solution.MakeEqual(words);
+Console.WriteLine(output); // true
+
+string[] words1 = ["ab", "a"];
+var output1 = solution.MakeEqual(words1);
+Console.WriteLine(output1); // false
+
+public class Solution
+{
+    public bool MakeEqual(string[] words)
+    {
+        int[] result = new int[26];
+
+        foreach (string word in words)
+        {
+            foreach (int c in word)
+            {
+                result[c - 'a']++;
+            }
+        }
+
+        return result.All(r => r % words.Length == 0);
+    }
+}
