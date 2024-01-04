ba58747f752c46c29bc873e5321b02340cc91fc7
PuTaoNiNi <oneheed@gmail.com>
2024-01-04 11:37:28 +0800

    Add 2125. Number of Laser Beams in a Bank
    Add 2870. Minimum Number of Operations to Make Array Empty

---
+++ solution/02125. Number of Laser Beams in a Bank/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+var solution = new Solution();
+
+string[] bank = ["011001", "000000", "010100", "001000"];
+var output = solution.NumberOfBeams(bank);
+Console.WriteLine(output); // 8
+
+string[] bank1 = ["000", "111", "000"];
+var output1 = solution.NumberOfBeams(bank1);
+Console.WriteLine(output1); // 8
+
+
+public class Solution
+{
+    public int NumberOfBeams(string[] bank)
+    {
+        var result = 0;
+        var left = 0;
+
+        foreach (var item in bank.Select(b => b.Count(c => c == '1')).Where(c => c != 0))
+        {
+            result += left * item;
+            left = item;
+        }
+
+        return result;
+    }
+}
---
+++ solution/02870. Minimum Number of Operations to Make Array Empty/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+var solution = new Solution();
+
+int[] nums = [2, 3, 3, 2, 2, 4, 2, 3, 4];
+var output = solution.MinOperations(nums);
+Console.WriteLine(output); // 4
+
+int[] nums1 = [2, 1, 2, 2, 3, 3];
+var output1 = solution.MinOperations(nums1);
+Console.WriteLine(output1); // -1
+
+public class Solution
+{
+    public int MinOperations(int[] nums)
+    {
+        var result = 0;
+        var intCount = new Dictionary<int, int>();
+
+        foreach (int i in nums)
+        {
+            if (intCount.ContainsKey(i))
+            {
+                intCount[i]++;
+            }
+            else
+            {
+                intCount[i] = 1;
+            }
+        }
+
+        foreach (int i in intCount.Select(i => i.Value))
+        {
+            if (i == 1)
+                return -1;
+
+            var d = i / 3;
+            var rem = i % 3 == 0 ? 0 : 1;
+            result += (d + rem);
+        }
+
+        return result;
+    }
+}
