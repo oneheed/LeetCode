933f062bd6bf43818cbd5874ba856fdaf5192b54
PuTaoNiNi <oneheed@gmail.com>
2024-01-04 17:01:23 +0800

    Add 19. Remove Nth Node From End of List
    Add 237. Delete Node in a Linked List

---
+++ solution/00019. Remove Nth Node From End of List/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Core;
+
+var solution = new Solution();
+
+var node = new ListNode(1, new(2, new(3, new(4, new(5)))));
+var n = 2;
+var result = solution.RemoveNthFromEnd(node, n);
+Console.WriteLine(result?.ToString()); // [1,2,3,5]
+
+var node1 = new ListNode(1);
+var n1 = 1;
+var result1 = solution.RemoveNthFromEnd(node1, n1);
+Console.WriteLine(result1?.ToString()); // []
+
+var node2 = new ListNode(1, new(2));
+var n2 = 1;
+var result2 = solution.RemoveNthFromEnd(node2, n2);
+Console.WriteLine(result2?.ToString()); // [1]
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var size = 0;
+        for (var i = head; i != null; i = i.next)
+            size++;
+
+        size -= n;
+
+        var cur = head;
+        for (var i = 0; i < size - 1; i++)
+            cur = cur.next;
+
+        if (size == 0)
+            return head.next;
+
+        cur.next = cur.next.next;
+
+        return head;
+    }
+
+    public ListNode RemoveNthFromEndHelper(ListNode head, ref int n)
+    {
+        if (head == null)
+            return null;
+
+        head.next = RemoveNthFromEndHelper(head.next, ref n);
+
+        n--;
+
+        if (n == 0)
+        {
+            return head.next;
+        }
+
+
+        return head;
+    }
+}
---
+++ solution/00237. Delete Node in a Linked List/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Core;
+
+var solution = new Solution();
+
+var node = new ListNode(5, new(1, new(9)));
+var head = new ListNode(4, node);
+solution.DeleteNode(node);
+Console.WriteLine(head.ToString()); // [4,1,9]
+
+
+var node1 = new ListNode(1, new(9));
+var head2 = new ListNode(4, new(5, node1));
+solution.DeleteNode(node1);
+Console.WriteLine(head2.ToString()); // [4,5,9]
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public void DeleteNode(ListNode node)
+    {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
